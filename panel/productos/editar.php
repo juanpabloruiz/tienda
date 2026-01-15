da714ed20e50b0da1412f0b0d616dbfe538eefc2
juanpabloruiz <pabloruiz1980@gmail.com>
2026-01-15 01:53:14 -0300

    Cambio fila por campo

--- panel/productos/editar.php
+++ panel/productos/editar.php
@@ -1,21 +1,21 @@
 <?php
 require_once __DIR__ . '/../../conexion.php';
 $id = $_GET['id'] ?? null;
-        $editar = null;
+        $campo = null;
 
         if ($id):
             $sentencia = $conexion->prepare("SELECT * FROM productos WHERE id = ?");
             $sentencia->bind_param("i", $id);
             $sentencia->execute();
             $resultado = $sentencia->get_result();
-            $editar = $resultado->fetch_assoc();
+            $campo = $resultado->fetch_assoc();
         endif;
 ?>
 
 <form method="POST" action="insertar.php">
-    <input type="hidden" name="id" value="<?= $editar['id'] ?? '' ?>">
-    <input type="text" name="producto" value="<?= $editar['producto'] ?? '' ?>">
-    <input type="text" name="precio" value="<?= $editar['precio'] ?? '' ?>">
+    <input type="hidden" name="id" value="<?= $campo['id'] ?? '' ?>">
+    <input type="text" name="producto" value="<?= $campo['producto'] ?? '' ?>">
+    <input type="text" name="precio" value="<?= $campo['precio'] ?? '' ?>">
     <input type="submit" name="insertar" value="Agregar">
 </form>
 
